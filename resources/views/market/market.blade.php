c5e7b0d0790680744f524f212216c5d99782439a
Kouhai <kouhai@localhost.localdomain>
2016-09-27 15:14:10 -0300

    + Adicionando view para market

---
+++ resources/views/market/market.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.mylayout')
+
+@section('styles')
+    <style>
+        .info{
+            font-size: 20px;
+        }
+
+        tr.click-row {
+            cursor: pointer;
+        }
+    </style>
+@endsection
+
+@section('title')
+    Produtos
+@endsection
+
+@section('content')
+
+    <div class="itens container">
+        @foreach($products as $product)
+
+            <div class='item col-md-3' style='margin-top:15px;margin-bottom:15px;'>
+                <img src='http://lorempixel.com/250/250/' class='img-responsive'>
+                <div class='descricao'>
+                    <h4 class='nome' data-nome='{{$product->nome}}'>{{$product->nome}}</h4>
+                    <p class='preco' data-preco='{{$product->nome}}'>R$ {{number_format($product->preco, 2, ',', ' ')}}</p>
+                    <button class='add-button btn btn-info'>Adicionar ao Carrinho</button>
+                </div>
+            </div>
+
+        @endforeach
+    </div>
+
+@endsection
+
+
+@section('scripts')
+    <script type="text/javascript" src="{{ asset('js/carrinho.js') }}"></script>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,6 @@ Auth::routes();
 Route::get('/home', 'HomeController@index');
 
 Route::get('/produtos', 'ProductsController@index')->name('produtos');
+Route::get('/market', 'MarketController@index')->name('market');
 
 Route::resource('estudantes', 'StudentController');
